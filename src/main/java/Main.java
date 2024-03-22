7140198e3933c866377bfbaf129d5a79c91c56eb
20Mphumzi16 <mphumzimbula@gmail.com>
2024-03-22 17:22:29 +0200

    Added Student class Code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,8 @@
+import za.ac.cput.domain.Student;
+
 public class Main {
+    public static void main(String[] args) {
+        Student student=new Student.StudentBuilder().setStudentNumber("219053324").setFirstName("mpumzi").build();
+        System.out.println(student);
+    }
 }
--- src/main/java/za/ac/cput/domain/Student.java
+++ src/main/java/za/ac/cput/domain/Student.java
@@ -1,4 +1,120 @@
 package za.ac.cput.domain;
 
 public class Student {
+    private String studentNumber;
+    private String idNumber;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String ethnicity;
+    private Contact contact;
+
+
+    private Student() {
+
+    }
+    private Student(StudentBuilder builder){
+        this.studentNumber= builder.studentNumber;
+        this.idNumber= builder.idNumber;
+        this.firstName= builder.firstName;
+        this.lastName= builder.lastName;
+        this.gender=builder.gender;
+        this.ethnicity= builder.ethnicity;
+        this.contact=builder.contact;
+
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getEthnicity() {
+        return ethnicity;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentNumber='" + studentNumber + '\'' +
+                ", idNumber='" + idNumber + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", ethnicity='" + ethnicity + '\'' +
+                ", contact=" + contact +
+                '}';
+    }
+
+    public static class StudentBuilder {
+        private String studentNumber;
+        private String idNumber;
+        private String firstName;
+        private String lastName;
+        private String gender;
+        private String ethnicity;
+        private Contact contact;
+
+
+
+        public StudentBuilder setStudentNumber(String studentNumber) {
+            this.studentNumber = studentNumber;
+            return this;
+        }
+
+        public StudentBuilder setIdNumber(String idNumber) {
+            this.idNumber = idNumber;
+            return this;
+        }
+
+        public StudentBuilder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public StudentBuilder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public StudentBuilder setGender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public StudentBuilder setEthnicity(String ethnicity) {
+            this.ethnicity = ethnicity;
+            return this;
+        }
+
+        public StudentBuilder setContact(Contact contact) {
+            this.contact = contact;
+            return this;
+        }
+
+        public Student build(){
+            return new Student(this);
+        }
+
+    }
+
 }
