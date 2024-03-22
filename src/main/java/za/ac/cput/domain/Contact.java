566854a23300e17cda838d92fcc054df9775484a
SETHU60 <promakhambeni@gmail.com>
2024-03-22 21:47:45 +0200

    Coded contact code

--- src/main/java/za/ac/cput/domain/Contact.java
+++ src/main/java/za/ac/cput/domain/Contact.java
@@ -1,4 +1,69 @@
 package za.ac.cput.domain;
-
+//Siphosethu Makhambeni
+//        221272976
+//22/03/2024
+//Contact.java
 public class Contact {
+    private String phoneNumber;
+    private String emailAddress;
+    private String address;
+
+    private Contact() {
+
+    }
+
+    private Contact(ContactBuilder builder) {
+        this.phoneNumber = builder.phoneNumber;
+        this.emailAddress = builder.emailAddress;
+        this.address = builder.address;
+    }
+
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "phoneNumber='" + phoneNumber + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+
+    public static class ContactBuilder {
+        private String phoneNumber;
+        private String emailAddress;
+        private String address;
+
+
+        public ContactBuilder setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public ContactBuilder setEmailAddress(String emailAddress) {
+            this.emailAddress = emailAddress;
+            return this;
+        }
+
+        public ContactBuilder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Contact build() {
+            return new Contact(this);
+        }
+    }
 }
