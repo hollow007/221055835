5bb40cd276e79a551c4abae45fe92daa2dfff2db
SETHU60 <promakhambeni@gmail.com>
2024-03-26 20:27:15 +0200

    ContactFactory, ContactFactoryTest, ContactHelper code done

--- src/main/java/za/ac/cput/factory/ContactFactory.java
+++ src/main/java/za/ac/cput/factory/ContactFactory.java
@@ -1,4 +1,25 @@
 package za.ac.cput.factory;
-
+import za.ac.cput.domain.Contact;
+import za.ac.cput.util.ContactHelper;
+//Siphosethu Makhambeni
+//        221272976
+//25/03/2024
+//ContactFactory
 public class ContactFactory {
+    public static Contact buildContact(String phoneNumber, String emailAddress, String address) {
+
+        if(ContactHelper.isNullOrEmpty(phoneNumber)||ContactHelper.isNullOrEmpty(emailAddress)||ContactHelper.isNullOrEmpty(address)){
+            return null;
+        }
+
+
+        return new Contact.ContactBuilder()
+                .setPhoneNumber(phoneNumber)
+                .setEmailAddress(emailAddress)
+                .setAddress(address)
+                .build();
+    }
+
+
+
 }
---
+++ src/main/java/za/ac/cput/util/ContactHelper.java
@@ -0,0 +1,19 @@
+package za.ac.cput.util;
+
+import za.ac.cput.domain.Contact;
+//Siphosethu Makhambeni
+//        221272976
+//25/03/2024
+//ContactFactory
+public class ContactHelper {
+   public static boolean isNullOrEmpty(String s){
+      if (s == null || s.isEmpty()){
+           return true;
+     }
+      return false;
+ }
+
+
+
+
+}
